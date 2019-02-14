d7bd8c924b3edac7e0953fc8c6dc02f75a8b8d48
“felipelm” <felipelm123@gmail.com>
2019-02-14 14:45:03 -0200

    keeping in memory

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM ubuntu
 RUN mkdir /app
 WORKDIR /app
 RUN apt-get update && apt-get install libvips-dev python3-pip -y
-RUN pip3 install matplotlib astropy
+RUN pip3 install matplotlib astropy pyvips
--- convertccds.py
+++ convertccds.py
@@ -3,9 +3,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import datetime
+import concurrent.futures
+import pyvips
+import io
 
-
-def save_image_tiff(fits_ccd_filename, ouputdir, idx):
+def save_image_tiff(fits_ccd_filename):
+    outputdir='./exps'
+    idx=fits_ccd_filename.split('/')[-1].split('det')[1].split('.fits')[0]
     print('Starting: ', fits_ccd_filename.split('/')[-1])
     start = datetime.datetime.now()
     hduccds = fits.open(fits_ccd_filename, memmap=True)  # open a FITS file
@@ -21,20 +25,32 @@ def save_image_tiff(fits_ccd_filename, ouputdir, idx):
     fig.add_axes(ax)
 
     ax.imshow(imgccd, vmin=1, vmax=200, cmap='gray')
-    plt.savefig("{}/mid.png".format(ouputdir), dpi=height)
-    plt.close()
-    print('Converting', idx)
+    buf = io.BytesIO()
+    plt.savefig(buf, dpi = height)
+    buf.seek(0)
+    image = pyvips.Image.new_from_buffer(buf.read(), "")
+    image.tiffsave("{}/{}.tif".format(outputdir, idx), tile=True, tile_width=256, tile_height=256, pyramid=True, bigtiff=True, compression='deflate')
 
-    os.system('vips im_vips2tiff {}/mid.png {}/{}.tif:deflate,tile:256x256,pyramid'.format(ouputdir, ouputdir, idx))
+    #plt.savefig("{}/{}.png".format(outputdir, idx), dpi=height)
+    plt.close()
+    #tile1 = pyvips.Image.new_from_file("{}/{}.png".format(outputdir, idx), access="sequential")
+    #tile1.tiffsave("{}/{}.tif".format(outputdir, idx), tile=True, tile_width=256, tile_height=256, pyramid=True, bigtiff=True)
+
+    #os.system('vips im_vips2tiff {}/{}.png {}/{}.tif:deflate,tile:256x256,pyramid'.format(outputdir, idx, outputdir, idx))
     end = datetime.datetime.now()
-    print('DONE: ', str(end-start))
+    #os.system('rm {}/{}.png'.format(outputdir, idx))
+    return 'DONE: {}'.format(str(end-start))
 
 
 import glob
 exps = glob.glob(
     '/home/felipe/repos/lsst/lsst_image/data_repo/calexp/00193824-g/R*/calexp*')
 start = datetime.datetime.now()
-for idx, exp in enumerate(exps):
-    save_image_tiff(exp, './exps', idx)
+with concurrent.futures.ProcessPoolExecutor() as executor:
+     for time in executor.map(save_image_tiff, exps):
+         print(time)
+
+#for idx, exp in enumerate(exps):
+#    save_image_tiff(exp, './exps', idx)
 end = datetime.datetime.now()
-print('DONE: ', str(end-start))
+print('DONEALL: ', str(end-start))
